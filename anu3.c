9a379b566b58cb736a0a5bb90dde1d3174a45368
anuu11 <35534904+anuu11@users.noreply.github.com>
2018-01-17 22:34:55 +0530

    Add files via upload

---
+++ anu3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char n;
+printf("enter character");
+scanf("%s",&n);
+
+
+if((n=='a')||(n=='e')||(n=='i')||(n=='o')||(n=='u'))
+printf("vowel");
+else
+printf("consonents");
+
+}
